e814d17723472e4422463d17d185d1adf72f01bc
oren.holis <oren.holis@bronet.cz>
2021-11-22 21:53:36 +0100

    Showing forecast for specific city

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,50 @@
+class Forecast extends Sword {
+	loadForecast(weather) {
+		this.el.innerHTML = '';
+
+		const forecast = this.createElement({
+			className: 'forecast-table',
+			children: weather.daily.map(d => {
+				// Multiplied with 1000 because of unix format is in seconds
+				const date = new Date(d.dt * 1000);
+				const language = navigator.language || navigator.userLanguage;
+				const dateFormatted = date.toLocaleDateString(language,  {
+					year: 'numeric',
+					month: 'numeric',
+					day: 'numeric',
+					hour12: false
+				})
+				const weekDay = date.toLocaleDateString(language,  {weekday: 'long'})
+
+				return {
+					className: 'day-forecast',
+					children: [{
+						textContent: dateFormatted,
+						className: 'date'
+					},{
+						textContent: weekDay,
+						className: 'week-day'
+					},{
+						className: 'temperature',
+						textContent: 'Day: ' + d.temp.day + ' °C',
+					},{
+						className: 'temperature',
+						textContent: 'Night: ' + d.temp.night + ' °C',
+					}]
+				}
+			})
+		})
+
+		this.el.appendChild(forecast);
+	}
+
+	render() {
+		this.el = this.createElement({
+			className: 'forecast'
+		})
+	}
+}
+
 class CityPicker extends Sword {
 	beforeRender() {
 		this.cities = new Map();
@@ -11,6 +58,7 @@ class CityPicker extends Sword {
 				placeholder: 'Enter city name',
 				'on:input': e => {
 					this.cityList.innerHTML = '';
+					this.cityList.className = 'city-list';
 					let citiesFiltered = [];
 
 					const inputTextClean = replaceSpecialLetters(e.target.value);
@@ -28,14 +76,19 @@ class CityPicker extends Sword {
 					for (const city of citiesFiltered) {
 						const cityEl = this.createElement({
 							textContent: city.name,
-							'on:click': () => this.fire('city', city)
+							'on:click': () => {
+								e.target.value = city.name;
+								this.hideWhisperer();
+								this.fire('city', city)
+							}
 						})
 
 						this.cityList.appendChild(cityEl);
 					}
 				}
 			},{
-				ref: 'cityList'
+				ref: 'cityList',
+				className: 'city-list invisible'
 			}]
 		}, this);
 
@@ -47,17 +100,38 @@ class CityPicker extends Sword {
 			}
 		});
 	}
+
+	hideWhisperer() {
+		this.cityList.className = 'invisible';
+		this.cityList.innerHTML = '';
+	}
 }
 
 class App extends Sword {
+	API_KEY = 'f571f988e21aef6443858a84a6dd757a';
+
+	async loadForecast(city) {
+		const data = await fetch(`https://api.openweathermap.org/data/2.5/
+			onecall?lat=${city.coord.lat}&lon=${city.coord.lon}&exclude=${city.part}&units=metric&appid=${this.API_KEY}`)
+		const weather = await data.json()
+
+		this.weatherShow.loadForecast(weather);
+	}
+
 	render() {
 		this.el = this.createElement({
 			className: 'app',
+			'on:click': () => this.cityPicker.hideWhisperer(),
 			children: [{
 				nodeName: 'h1',
 				textContent: 'Weather forecast'
 			},{
-				class: CityPicker
+				class: CityPicker,
+				'on:city': (obj, city) => this.loadForecast(city),
+				ref: 'cityPicker'
+			},{
+				class: Forecast,
+				ref: 'weatherShow'
 			}]
 		}, this)
 	}
