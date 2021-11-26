4965ac91d1e246b1226c2ae306451addccc3e3bb
oren.holis <oren.holis@bronet.cz>
2021-11-26 18:17:12 +0100

    Adding city whisper

--- react-weather-forecast/src/index.jsx
+++ react-weather-forecast/src/index.jsx
@@ -1,16 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CityPicker from "./scripts/citywhisperer";
 import './styles/index.css';
 
 class ReactApp extends React.Component {
 	constructor(props) {
 		super(props);
+
+		this.state = {
+			forecast: null,
+			api_key: 'your api key'
+		}
+
+		this.loadForecast = this.handleForecastLoad.bind(this);
+	}
+
+	async handleForecastLoad(city) {
+		this.setState({forecast: null})
+		const data = await fetch(`https://api.openweathermap.org/data/2.5/
+			onecall?lat=${city.coord.lat}&lon=${city.coord.lon}&exclude=${city.part}&units=metric&appid=${this.state.api_key}`)
+		const forecast = await data.json()
+
+		this.setState({forecast})
 	}
 
 	render() {
 		return(
 			<div>
-				React app
+				<CityPicker cb={this.loadForecast}/>
 			</div>
 		)
 	}
---
+++ react-weather-forecast/src/scripts/citywhisperer.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+import {readTextFile, replaceSpecialLetters} from "./utils";
+
+export default class CityPicker extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			cities: [],
+			citiesLoaded: new Map()
+		}
+
+		this.filter = this.filterCities.bind(this);
+		this.retrieveJSON();
+	}
+
+	filterCities(e) {
+		let citiesFiltered = [];
+
+		const inputTextClean = replaceSpecialLetters(e.target.value);
+
+		for (const [k, v] of this.state.citiesLoaded) {
+			if (citiesFiltered.length === 15) {
+				break;
+			}
+
+			if (k.startsWith(inputTextClean)) {
+				citiesFiltered.push(v)
+			}
+		}
+
+		this.setState({cities: citiesFiltered})
+	}
+
+	retrieveJSON() {
+		readTextFile("city.list.json", (json) => {
+			const myWorker = new Worker("./workers/worker.js");
+
+			myWorker.postMessage({
+				json,
+				replaceSpecialLetters: JSON.stringify(replaceSpecialLetters, (k, v) => {
+					return v.toString().replaceAll('\n', '')
+				})
+			});
+
+			myWorker.onmessage = function(e) {
+				this.setState({citiesLoaded: e.data});
+			}.bind(this);
+		});
+	}
+
+	selectCity(city) {
+		console.log(city)
+		this.props.cb(city);
+		this.setState({cities: []})
+	}
+
+	render() {
+		return (
+			<div className={'city-picker'}>
+				<input
+					disabled={this.state.citiesLoaded.size === 0}
+					placeholder={this.state.citiesLoaded.size === 0 ? 'List of cities is being loaded...' : 'Enter city name'}
+					onChange={this.filter}
+				/>
+				{
+					this.state.cities.length > 0 &&
+					<div className={'city-list'}>
+						{
+							this.state.cities.map((c, i) => {
+								return (
+									<div key={i} onClick={() => this.selectCity(c)}>{c.name}</div>
+								)
+							})
+						}
+					</div>
+				}
+			</div>
+		)
+	}
+}
